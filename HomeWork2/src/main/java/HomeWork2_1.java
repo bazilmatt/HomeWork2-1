8c18d177d6b781062892fcbf744fe8a976036e12
Vasiliy Matveev <bazilmat@yandex.ru>
2020-04-23 15:51:42 +0300

    First part of homework for lesson 2 - updated

--- HomeWork2/src/main/java/HomeWork2_1.java
+++ HomeWork2/src/main/java/HomeWork2_1.java
@@ -7,9 +7,9 @@ public class HomeWork2_1 {
     public static void main(String[] args) {
         Object b = null;
         try {
-            System.out.println(b.toString());
+            System.out.println("Hello world" + b.toString());
         } catch (NullPointerException e){
-            System.out.println("пустое переданное значение - отработал NPE");
+            System.out.println("в переменной b значение null - вызван обработчик исключения");
         } finally {
             System.out.println("а также отработал блок finally"); //можно убрать, здесь незачем
         }
